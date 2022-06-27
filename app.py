6b6b3d8b1b5bd274cfe3cf5be6625826bf03958b
noob04coder <78252850+noob04coder@users.noreply.github.com>
2022-06-27 15:06:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,52 +1,64 @@
 import asyncio
+from urllib.parse import urlparse
+
 import aiohttp
 
-from proxybroker import Broker
+from proxybroker import Broker, ProxyPool
+from proxybroker.errors import NoProxyError
 
 
-async def get_pages(urls, proxy_url):
-    tasks = [fetch(url, proxy_url) for url in urls]
+async def get_pages(urls, proxy_pool, timeout=10, loop=None):
+    tasks = [fetch(url, proxy_pool, timeout, loop) for url in urls]
     for task in asyncio.as_completed(tasks):
         url, content = await task
-        print('Done! url: %s; content: %.100s' % (url, content))
+        print('Done! url: %s; content: %.30s' % (url, content))
 
 
-async def fetch(url, proxy_url):
-    resp = None
+async def fetch(url, proxy_pool, timeout, loop):
+    resp, proxy = None, None
     try:
-        async with aiohttp.ClientSession() as session:
-            async with session.get(url, proxy=proxy_url) as response:
-                resp = await response.read()
+        proxy = await proxy_pool.get(scheme=urlparse(url).scheme)
+        proxy_url = 'http://%s:%d' % (proxy.host, proxy.port)
+        with aiohttp.Timeout(timeout, loop=loop):
+            async with aiohttp.ClientSession(loop=loop) as session:
+                async with session.get(url, proxy=proxy_url) as response:
+                    resp = await response.read()
     except (aiohttp.errors.ClientOSError, aiohttp.errors.ClientResponseError,
-            aiohttp.errors.ServerDisconnectedError) as e:
-        print('Error. url: %s; error: %r' % (url, e))
+            aiohttp.errors.ServerDisconnectedError, asyncio.TimeoutError,
+            NoProxyError) as e:
+        print('Error. url: %s; error: %r', url, e)
     finally:
+        if proxy:
+            proxy_pool.put(proxy)
         return (url, resp)
 
 
 def main():
-    host, port = '127.0.0.1', 8888  # by default
-
     loop = asyncio.get_event_loop()
 
-    types = [('HTTP', 'High'), 'HTTPS', 'CONNECT:80']
-    codes = [200, 301, 302]
+    proxies = asyncio.Queue(loop=loop)
+    proxy_pool = ProxyPool(proxies)
 
-    broker = Broker(max_tries=1, loop=loop)
+    judges = ['http://httpbin.org/get?show_env',
+              'https://httpbin.org/get?show_env']
+    providers = ['http://www.proxylists.net/', 'http://fineproxy.org/eng/fresh-proxies/']
 
-    # Broker.serve() also supports all arguments that are accepted
-    # Broker.find() method: data, countries, post, strict, dnsbl.
-    broker.serve(host=host, port=port, types=types, limit=10, max_tries=3,
-                 prefer_connect=True, min_req_proxy=5, max_error_rate=0.5,
-                 max_resp_time=8, http_allowed_codes=codes, backlog=100)
+    broker = Broker(
+        proxies, timeout=8, max_conn=200, max_tries=3, verify_ssl=False,
+        judges=judges, providers=providers, loop=loop)
 
-    urls = ['http://httpbin.org/get', 'https://httpbin.org/get',
-            'http://httpbin.org/redirect/1', 'http://httpbin.org/status/404']
+    types = [('HTTP', ('Anonymous', 'High')), ]
+    countries = ['US', 'DE', 'FR']
 
-    proxy_url = 'http://%s:%d' % (host, port)
-    loop.run_until_complete(get_pages(urls, proxy_url))
+    urls = ['http://httpbin.org/get', 'http://httpbin.org/redirect/1',
+            'http://httpbin.org/anything', 'http://httpbin.org/status/404']
 
-    broker.stop()
+    tasks = asyncio.gather(
+        broker.find(types=types, countries=countries, strict=True, limit=10),
+        get_pages(urls, proxy_pool, loop=loop))
+    loop.run_until_complete(tasks)
+
+    # broker.show_stats(verbose=True)
 
 
 if __name__ == '__main__':
